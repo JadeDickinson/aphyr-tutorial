9f9b0b2343ad8e415584f891795518e083043547
Jade Dickinson <jadedickinson@googlemail.com>
2019-05-01 17:57:44 +0100

    First commit

---
+++ src/aphyr_tutorial/core.clj
@@ -0,0 +1,20 @@
+(ns aphyr-tutorial.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (str x "Hello, World!"))
+
+(foo "a")
+
+; Starting from https://aphyr.com/posts/301-clojure-from-the-ground-up-welcome and simply running the code
+nil
+true
+false
+0
+"hi there"
+inc
+'inc
+'123
+'"foo"
+'(1 2 3)
---
+++ test/aphyr_tutorial/core_test.clj
@@ -0,0 +1,7 @@
+(ns aphyr-tutorial.core-test
+  (:require [clojure.test :refer :all]
+            [aphyr-tutorial.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
